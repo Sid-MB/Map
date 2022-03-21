c79caeb12e3d939975182766adbba8678c5d1d88
Sid-MB <siddharth.m.bhatia@gmail.com>
2022-03-20 18:09:34 -0700

    Using the URL API works beautifully

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import './style.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const MAPBOX_API_TOKEN =
+  'pk.eyJ1IjoiZm9vZmxlIiwiYSI6ImNsMHkwMWJmeTF0ZTczaXJwdXJmdGR0cWcifQ.HuJqUcujJniVQ1XEFDXluQ';
+
+const locations = {
+  school: [-122.233, 37.823],
+  garden: [-122.469, 37.769],
+};
+
 // MARK:- THE MAP
 var mapboxgl = require('mapbox-gl');
-var MapboxDirections = require('@mapbox/mapbox-gl-directions');
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiZm9vZmxlIiwiYSI6ImNsMHkwMWJmeTF0ZTczaXJwdXJmdGR0cWcifQ.HuJqUcujJniVQ1XEFDXluQ';
+mapboxgl.accessToken = MAPBOX_API_TOKEN;
 
 const map = new mapboxgl.Map({
   container: 'map',
@@ -15,33 +21,29 @@ const map = new mapboxgl.Map({
   zoom: 9,
 });
 
-const directions = new MapboxDirections({
-  accessToken:
-    'pk.eyJ1IjoiZm9vZmxlIiwiYSI6ImNsMHkwMWJmeTF0ZTczaXJwdXJmdGR0cWcifQ.HuJqUcujJniVQ1XEFDXluQ',
-  profile: 'mapbox/driving',
-  interactive: false,
-  controls: {
-    inputs: false,
-    instructions: false,
-    profileSwitcher: false,
-  },
-});
+// MARK:- Get Path using URL API
+function getPathURLAPI() {
+  const coordinateString = Object.values(locations)
+    .map((pair) => pair.join(','))
+    .join(';');
+  const fetchURL = `https://api.mapbox.com/directions/v5/mapbox/driving/${coordinateString}?alternatives=false&geometries=polyline&overview=full&steps=false&access_token=${MAPBOX_API_TOKEN}`;
+  console.log('Fetching:', fetchURL);
+  async function request() {
+    try {
+      const response = await fetch(fetchURL); // https://docs.mapbox.com/api/navigation/directions/#example-requests-retrieve-directions
 
-directions.on('route', (result) => {
-  const polyline = result.route[0].geometry;
-  console.log(polyline);
-});
+      const data = await response.json();
 
-const locations = {
-  school: [-122.233, 37.823],
-  garden: [-122.469, 37.769],
-};
-map.on('load', () => {
-  //map.addControl(directions);
+      const polyline = data.routes[0].geometry;
+      console.log('Mapbox URL API', polyline);
+    } catch (error) {
+      console.error('Error retrieving directions', error);
+    }
+  }
+  request();
+}
 
-  directions.setOrigin(locations.school); // can be address in form setOrigin("12, Elm Street, NY")
-  directions.setDestination(locations.garden); // can be address
-});
+getPathURLAPI();
 
 // Light/Dark mode
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
